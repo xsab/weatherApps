f4c15fdba5c7a83af159774e82682b18a718563d
xsab <81972587+xsab@users.noreply.github.com>
2021-05-08 19:04:55 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Weather from '/components/Weather';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
@@ -6,17 +7,48 @@ export default class App extends React.Component {
 		isLoading: true
 	};
 
+  componentDidMount() {
+		navigator.geolocation.getCurrentPosition(
+			position => {
+				this.fetchWeather(position.coords.latitude, position.coords.longitude);
+			},
+			error => {
+				this.setState({
+					error: 'Error Getting Weather Conditions'
+				});
+			}
+		);
+	}
+
+	fetchWeather(lat = 25, lon = 25) {
+		fetch(
+			`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&APPID=${API_KEY}&units=metric`
+		)
+			.then(res => res.json())
+			.then(json => {
+				this.setState({
+					temperature: json.main.temp,
+					weatherCondition: json.weather[0].main,
+					isLoading: false
+				});
+			});
+	}
+
 	render() {
 		const { isLoading } = this.state;
 		return (
 			<View style={styles.container}>
-				{isLoading ? (
-           <Text>Fetching The Weather</Text>
+				state = {
+             isLoading: false,
+             temperature: 0,
+             weatherCondition: null,
+	        	 error: null? 
+            <Text>Fetching The Weather</Text> : <Weather />}
         ) : (
 					<View>
 						<Text>Minimalist Weather App</Text>
 					</View>
-				)}
+				)
 			</View>
 		);
 	}
